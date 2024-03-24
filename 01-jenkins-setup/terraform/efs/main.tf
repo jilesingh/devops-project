974f880fca26abaaf3e3991a651718f847bc330b
jilesingh <itsjilesingh@gmail.com>
2024-03-24 20:36:04 +0100

    provisioning using terraform

--- 01-jenkins-setup/terraform/efs/main.tf
+++ 01-jenkins-setup/terraform/efs/main.tf
@@ -5,5 +5,5 @@ provider "aws" {
 module "efs_module" {
   source = "../modules/efs"
   vpc_id     = "vpc-03778c84e7b794c88"
-  subnet_ids = ["subnet-0f26e5fdb292a8fd4", "subnet-00db75f88b2b9cf03", ""]
+  subnet_ids = ["subnet-0f26e5fdb292a8fd4", "subnet-00db75f88b2b9cf03", "subnet-032c00bbd686d3e6b"]
 }
